d0e0b27228ef07fbadf6df041bca188d819e4af3
JanghoPark <pjhsk113@naver.com>
2021-08-22 16:47:13 +0900

    feat: custom exception 반환 status code 추가

--- src/main/java/study/backend/realworld/application/user/domain/exception/DuplicateEmailException.java
+++ src/main/java/study/backend/realworld/application/user/domain/exception/DuplicateEmailException.java
@@ -1,7 +1,11 @@
 package study.backend.realworld.application.user.domain.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import javax.transaction.SystemException;
 
+@ResponseStatus(HttpStatus.CONFLICT)
 public class DuplicateEmailException extends SystemException {
     public DuplicateEmailException(String message) {
         super(message);
