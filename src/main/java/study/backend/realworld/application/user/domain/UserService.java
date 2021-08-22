b9bdd373fec6108c2c5df8a22c56384ac2cbd874
JanghoPark <pjhsk113@naver.com>
2021-08-22 16:49:02 +0900

    feat: User Update 구현
    - custom exception 코드 추가

--- src/main/java/study/backend/realworld/application/user/domain/UserService.java
+++ src/main/java/study/backend/realworld/application/user/domain/UserService.java
@@ -2,8 +2,13 @@ package study.backend.realworld.application.user.domain;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import study.backend.realworld.application.user.domain.exception.DuplicateEmailException;
+import study.backend.realworld.application.user.domain.exception.PasswordNotMatchedException;
+import study.backend.realworld.application.user.domain.exception.UserNotFountException;
+import study.backend.realworld.application.user.domain.model.UpdateProfileModel;
 import study.backend.realworld.application.user.domain.model.UserLoginModel;
 import study.backend.realworld.application.user.domain.model.UserRegisterModel;
+import study.backend.realworld.application.user.web.request.UpdateProfileRequest;
 
 @Service
 public class UserService {
@@ -15,20 +20,43 @@ public class UserService {
         this.passwordEncoder = passwordEncoder;
     }
 
-    public User register(UserRegisterModel request) {
+    public User register(UserRegisterModel request) throws Exception {
+        if (!userRepository.findByEmail(request.getEmail()).isEmpty()) {
+            throw new DuplicateEmailException("duplicate email");
+        }
+
         String encodedPassword = passwordEncoder.encode(request.getPassword());
         return userRepository.save(User.of(request.getUsername(),
                 request.getEmail(),
                 encodedPassword));
     }
 
-    public User login(UserLoginModel request) {
-        return userRepository.findByEmail(request.getEmail())
-                .orElseThrow(() -> new IllegalArgumentException("email not found"));
+    public User login(UserLoginModel request) throws Exception {
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(UserNotFountException::new);
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new PasswordNotMatchedException();
+        }
+
+        return user;
     }
 
-    public User find(User user) {
-        return userRepository.findById(user.getId()).orElseThrow(
-                () -> new IllegalArgumentException("user not found"));
+    public User find(User user) throws UserNotFountException {
+        return userRepository.findById(user.getId())
+                .orElseThrow(UserNotFountException::new);
     }
+
+    public User updateProfile(User user, UpdateProfileModel request) throws Exception {
+        if (!userRepository.findByEmail(request.getEmail()).isEmpty()) {
+            throw new DuplicateEmailException("duplicate email");
+        }
+
+        User updateUser = userRepository.findById(user.getId()).orElseThrow(UserNotFountException::new);
+        updateUser.updateProfile(request.getUsername(), new Profile(request), user.getPassword());
+
+        return userRepository.saveAndFlush(updateUser);
+
+    }
+
 }
