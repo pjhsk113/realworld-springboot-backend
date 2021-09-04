aa81fe5651e8b1504916b75bed3a50e3f86b465f
JanghoPark <pjhsk113@naver.com>
2021-09-04 18:37:19 +0900

    feat: Follow Controller endpoint 추가
    - unfollow, find 추가

--- src/main/java/study/backend/realworld/application/user/web/FollowRestController.java
+++ src/main/java/study/backend/realworld/application/user/web/FollowRestController.java
@@ -2,10 +2,7 @@ package study.backend.realworld.application.user.web;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import study.backend.realworld.application.user.domain.User;
 import study.backend.realworld.application.user.web.response.ProfileResponse;
 
@@ -16,6 +13,18 @@ public class FollowRestController {
     @PostMapping("/{username}/follow")
     public ResponseEntity<ProfileResponse> following(@AuthenticationPrincipal User user,
                                                      @PathVariable String userName) {
+        return ResponseEntity.ok(null);
+    }
+
+    @DeleteMapping("/{username}/follow")
+    public ResponseEntity<ProfileResponse> unFollow(@AuthenticationPrincipal User user,
+                                                    @PathVariable String userName) {
+        return ResponseEntity.ok(null);
+    }
+
+    @GetMapping("/{username}/follow")
+    public ResponseEntity<ProfileResponse> find(@AuthenticationPrincipal User user,
+                                                @PathVariable String userName) {
         return ResponseEntity.ok(null);
     }
 }
