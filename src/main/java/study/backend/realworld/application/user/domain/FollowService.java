8c41db5638571274ecb5b7a96edfaee643b2417d
JanghoPark <pjhsk113@naver.com>
2021-09-19 17:53:13 +0900

    feat: follow 기능 구현

---
+++ src/main/java/study/backend/realworld/application/user/domain/FollowService.java
@@ -0,0 +1,19 @@
+package study.backend.realworld.application.user.domain;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import study.backend.realworld.application.user.domain.exception.ExistsUserException;
+import study.backend.realworld.application.user.domain.exception.UserNotFountException;
+
+@Service
+@RequiredArgsConstructor
+public class FollowService {
+    private UserRepository userRepository;
+
+    public Profile follow(User user, String userName) throws UserNotFountException, ExistsUserException {
+        User target = userRepository.findByUsername(userName)
+                .orElseThrow(UserNotFountException::new);
+        user.follow(target);
+        return new Profile(userName);
+    }
+}
