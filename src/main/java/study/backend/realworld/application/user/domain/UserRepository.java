ea81e7a1f10f5319d4de6bd57d15440c8837a81f
JanghoPark <pjhsk113@naver.com>
2021-09-19 17:52:00 +0900

    feat: userName 검색 기능 추가

--- src/main/java/study/backend/realworld/application/user/domain/UserRepository.java
+++ src/main/java/study/backend/realworld/application/user/domain/UserRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     User save(User user);
     Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
 }
