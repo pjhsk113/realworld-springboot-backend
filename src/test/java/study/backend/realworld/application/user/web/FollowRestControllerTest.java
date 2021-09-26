9c3cfba71ca3690c2681252ff252f5b1f0258fb9
JanghoPark <pjhsk113@naver.com>
2021-09-26 17:48:31 +0900

    test: FollowRestController - follow 관련 슬라이스 테스트

--- src/test/java/study/backend/realworld/application/user/web/FollowRestControllerTest.java
+++ src/test/java/study/backend/realworld/application/user/web/FollowRestControllerTest.java
@@ -1,10 +1,14 @@
 package study.backend.realworld.application.user.web;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import study.backend.realworld.application.IntegrationTests;
 import study.backend.realworld.application.user.domain.User;
+import study.backend.realworld.application.user.domain.exception.ExistsUserException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class FollowRestControllerTest extends IntegrationTests {
 
@@ -13,6 +17,29 @@ class FollowRestControllerTest extends IntegrationTests {
     @BeforeEach
     protected void setUp(){
         super.setUp();
-        user = userRepository.save(User.of("aaa@email.com", "aaa", "password"));
+        user = userRepository.save(User.of("test@email.com", "test", "password"));
     }
+
+    @Test
+    void when_follow_success() throws Exception {
+        String username = "test";
+
+        mockMvc.perform(post("/api/profiles/" + username + "/follow")
+                .contentType(APPLICATION_JSON)
+                .header(AUTHORIZATION, setUpToken))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void when_follow_fail_exist_user() throws Exception {
+        setUpUser.follow(user);
+
+        String username = "test";
+
+        mockMvc.perform(post("/api/profiles/" + username + "/follow")
+                .contentType(APPLICATION_JSON)
+                .header(AUTHORIZATION, setUpToken))
+                .andExpect(status().isBadRequest());
+    }
+
 }
