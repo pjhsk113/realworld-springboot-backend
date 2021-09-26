8e61b9aed507c6fb55729f57ecddd9c0d070ff7b
JanghoPark <pjhsk113@naver.com>
2021-09-26 17:47:04 +0900

    fix: PathVariable 이름 오타 수정
    - userName -> username으로 변경

--- src/main/java/study/backend/realworld/application/user/web/FollowRestController.java
+++ src/main/java/study/backend/realworld/application/user/web/FollowRestController.java
@@ -19,20 +19,20 @@ public class FollowRestController {
     private final FollowService followService;
 
     @PostMapping("/{username}/follow")
-    public ResponseEntity<ProfileResponse> following(@AuthenticationPrincipal User user, @PathVariable String userName) throws ExistsUserException, UserNotFountException {
-        Profile profile = followService.follow(user, userName);
+    public ResponseEntity<ProfileResponse> following(@AuthenticationPrincipal User user, @PathVariable String username) throws ExistsUserException, UserNotFountException {
+        Profile profile = followService.follow(user, username);
 
         return ResponseEntity.ok(ProfileResponse.of(profile));
     }
 
     @DeleteMapping("/{username}/unfollow")
-    public ResponseEntity<ProfileResponse> unFollow(@AuthenticationPrincipal User user, @PathVariable String userName) throws UserNotFountException {
-        Profile profile = followService.unfollow(user, userName);
+    public ResponseEntity<ProfileResponse> unFollow(@AuthenticationPrincipal User user, @PathVariable String username) throws UserNotFountException {
+        Profile profile = followService.unfollow(user, username);
         return ResponseEntity.ok(ProfileResponse.of(profile));
     }
 
     @GetMapping("/{username}")
-    public ResponseEntity<ProfileResponse> find(@AuthenticationPrincipal User user, @PathVariable String userName) {
+    public ResponseEntity<ProfileResponse> find(@AuthenticationPrincipal User user, @PathVariable String username) {
         return ResponseEntity.ok(null);
     }
 }
