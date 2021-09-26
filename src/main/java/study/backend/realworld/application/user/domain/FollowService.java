d7ba9328a8b1850336e1fa4b8e8e060d3c6464d5
JanghoPark <pjhsk113@naver.com>
2021-09-26 17:30:32 +0900

    refactor: username 찾는 메서드명 변경

--- src/main/java/study/backend/realworld/application/user/domain/FollowService.java
+++ src/main/java/study/backend/realworld/application/user/domain/FollowService.java
@@ -1,24 +1,26 @@
 package study.backend.realworld.application.user.domain;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import study.backend.realworld.application.user.domain.exception.ExistsUserException;
 import study.backend.realworld.application.user.domain.exception.UserNotFountException;
 
 @Service
-@RequiredArgsConstructor
 public class FollowService {
     private UserRepository userRepository;
 
+    public FollowService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public Profile follow(User user, String userName) throws UserNotFountException, ExistsUserException {
-        User target = userRepository.findByUsername(userName)
+        User target = userRepository.findByProfileUsername(userName)
                 .orElseThrow(UserNotFountException::new);
         user.follow(target);
         return new Profile(userName);
     }
 
     public Profile unfollow(User user, String userName) throws UserNotFountException {
-        User target = userRepository.findByUsername(userName)
+        User target = userRepository.findByProfileUsername(userName)
                 .orElseThrow(UserNotFountException::new);
         user.unfollow(target);
         return new Profile(userName);
