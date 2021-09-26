4ef84fc5c508ce400f2c4468f323fd1809183800
JanghoPark <pjhsk113@naver.com>
2021-09-26 17:46:23 +0900

    feat: Exception Response statusCode 추가

--- src/main/java/study/backend/realworld/application/user/domain/exception/DuplicateUserNameException.java
+++ src/main/java/study/backend/realworld/application/user/domain/exception/DuplicateUserNameException.java
@@ -1,7 +1,11 @@
 package study.backend.realworld.application.user.domain.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import javax.transaction.SystemException;
 
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class DuplicateUserNameException extends SystemException {
     public DuplicateUserNameException(String message) {
         super(message);
--- src/main/java/study/backend/realworld/application/user/domain/exception/ExistsUserException.java
+++ src/main/java/study/backend/realworld/application/user/domain/exception/ExistsUserException.java
@@ -1,7 +1,11 @@
 package study.backend.realworld.application.user.domain.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import javax.transaction.SystemException;
 
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ExistsUserException extends SystemException {
     public ExistsUserException() {
         super("exists follow user");
