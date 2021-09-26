325a97ee73d0a01213c97b9e604c0d2e48e20d71
JanghoPark <pjhsk113@naver.com>
2021-09-26 17:29:45 +0900

    fix: Profile의 username을 못찾던 문제 수정
    - Embedded의 이름을 명시해 username을 찾을 수 있도록 변경
    findByUsername -> findByProfileUsername

--- src/main/java/study/backend/realworld/application/user/domain/UserRepository.java
+++ src/main/java/study/backend/realworld/application/user/domain/UserRepository.java
@@ -1,11 +1,13 @@
 package study.backend.realworld.application.user.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User save(User user);
     Optional<User> findByEmail(String email);
-    Optional<User> findByUsername(String username);
+    Optional<User> findByProfileUsername(String username);
 }
