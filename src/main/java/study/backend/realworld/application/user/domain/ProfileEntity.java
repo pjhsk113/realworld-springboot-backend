40bae02d3d2a6af25912cdc1ac2028dbb830ccbe
JanghoPark <pjhsk113@naver.com>
2022-01-12 20:15:07 +0900

    feat: Profile Entity 추가
    - Embedded를 최대한 활용해보자

---
+++ src/main/java/study/backend/realworld/application/user/domain/ProfileEntity.java
@@ -0,0 +1,36 @@
+package study.backend.realworld.application.user.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Embedded;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class ProfileEntity {
+    @Embedded
+    private UserName userName;
+
+    @Column(name = "bio")
+    private String bio;
+
+    @Embedded
+    private Image image;
+
+    private boolean following;
+
+    private ProfileEntity(UserName userName, String bio, Image image, boolean following) {
+        this.userName = userName;
+        this.bio = bio;
+        this.image = image;
+        this.following = following;
+    }
+
+    public ProfileEntity(UserName userName) {
+        this(userName, null, null, false);
+    }
+}
