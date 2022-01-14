a06d7bf9bcd022d8b590b0cb5cd3e7c9b022da42
JanghoPark <pjhsk113@naver.com>
2022-01-14 21:47:48 +0900

    refactor: controller 매핑 객체 변경

--- src/main/java/study/backend/realworld/application/user/api/UserRestController.java
+++ src/main/java/study/backend/realworld/application/user/api/UserRestController.java
@@ -4,13 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import study.backend.realworld.application.user.domain.User;
 import study.backend.realworld.application.user.application.UserService;
-import study.backend.realworld.application.user.exception.UserNotFountException;
+import study.backend.realworld.application.user.domain.UserEntity;
 import study.backend.realworld.application.user.dto.request.LoginRequest;
 import study.backend.realworld.application.user.dto.request.RegisterRequest;
 import study.backend.realworld.application.user.dto.request.UpdateProfileRequest;
 import study.backend.realworld.application.user.dto.response.UserResponse;
+import study.backend.realworld.application.user.exception.UserNotFountException;
 import study.backend.realworld.infra.security.jwt.TokenGenerator;
 
 import javax.validation.Valid;
@@ -32,14 +32,14 @@ public class UserRestController {
 
     @PostMapping("/users/login")
     public ResponseEntity<UserResponse> login(@RequestBody @Valid LoginRequest request) throws Exception {
-        User user = userService.login(request.toLoginRequest());
+        UserEntity user = userService.login(request.toLoginRequest());
         String token = tokenGenerator.generateToken(user.getEmail());
 
         return ResponseEntity.ok(UserResponse.of(user, token));
     }
 
     @GetMapping("/user")
-    public ResponseEntity<UserResponse> find(@AuthenticationPrincipal User user,
+    public ResponseEntity<UserResponse> find(@AuthenticationPrincipal UserEntity user,
                                              @RequestHeader("Authorization") String authorization) throws UserNotFountException {
         return ResponseEntity.ok(
                 UserResponse.of(userService.find(user), authorization.split(" ")[1])
@@ -47,7 +47,7 @@ public class UserRestController {
     }
 
     @PutMapping("/user")
-    public ResponseEntity<UserResponse> update(@AuthenticationPrincipal User user,
+    public ResponseEntity<UserResponse> update(@AuthenticationPrincipal UserEntity user,
                                                @RequestBody @Valid UpdateProfileRequest profile,
                                                @RequestHeader("Authorization") String authorization) throws Exception {
 
