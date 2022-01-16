b3f0a8a5a9573d43b4ff50cc55df8a8d73fd510c
JanghoPark <pjhsk113@naver.com>
2022-01-16 13:56:37 +0900

    feat: Email equals and hashCode 추가

--- src/main/java/study/backend/realworld/application/user/domain/Email.java
+++ src/main/java/study/backend/realworld/application/user/domain/Email.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -18,4 +19,16 @@ public class Email {
         this.email = email;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Email email1 = (Email) o;
+        return Objects.equals(email, email1.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email);
+    }
 }
