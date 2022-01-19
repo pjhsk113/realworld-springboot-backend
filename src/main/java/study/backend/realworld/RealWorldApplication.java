a12407e3d5c9101e767a6076d167c46a72332798
JanghoPark <pjhsk113@naver.com>
2022-01-19 16:08:16 +0900

    feat: JPA Auditing 설정 추가

--- src/main/java/study/backend/realworld/RealWorldApplication.java
+++ src/main/java/study/backend/realworld/RealWorldApplication.java
@@ -2,8 +2,10 @@ package study.backend.realworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class RealWorldApplication {
 
     public static void main(String[] args) {
