04ca02aba331f05fdbe83a3018ba38167d968832
JanghoPark <pjhsk113@naver.com>
2022-01-19 16:10:35 +0900

    feat: Article 세부 항목 추가(TODO Comment)
    - 연과 관계 설정
    - JPA Auditing 추가
    - favorite 기능 추가

--- src/main/java/study/backend/realworld/application/article/domain/Article.java
+++ src/main/java/study/backend/realworld/application/article/domain/Article.java
@@ -3,14 +3,21 @@ package study.backend.realworld.application.article.domain;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import study.backend.realworld.application.user.domain.User;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Table(name = "articles")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Article {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
@@ -20,8 +27,33 @@ public class Article {
     @ManyToOne(fetch = FetchType.EAGER)
     private User author;
 
-    // ArticleTitle - slug, titile
-    // ArticleContents - body, description, tags
-    // favorited
-    // Comments
+    @Embedded
+    private ArticleContents contents;
+
+    @JoinTable(name = "article_favorites",
+            joinColumns = @JoinColumn(name = "article_id", referencedColumnName = "id", nullable = false),
+            inverseJoinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false))
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
+    private Set<User> userFavorited = new HashSet<>();
+
+    private boolean favorited = false;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    // TODO Comments
+
+    public Article(User author, ArticleContents contents) {
+        this.author = author;
+        this.contents = contents;
+    }
+
+    public int getFavoritedCount() {
+        return userFavorited.size();
+    }
 }
