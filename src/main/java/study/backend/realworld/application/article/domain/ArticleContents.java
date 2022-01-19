193191021b72b5b3b59839a4b5ae9901fdb9f4fc
JanghoPark <pjhsk113@naver.com>
2022-01-19 16:07:24 +0900

    feat: ArticleContents 추가

---
+++ src/main/java/study/backend/realworld/application/article/domain/ArticleContents.java
@@ -0,0 +1,38 @@
+package study.backend.realworld.application.article.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class ArticleContents {
+
+    @Embedded
+    private ArticleTitle title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String body;
+
+    @JoinTable(name = "articles_tags",
+            joinColumns = @JoinColumn(name = "article_id", referencedColumnName = "id", nullable = false),
+            inverseJoinColumns = @JoinColumn(name = "tag_id", referencedColumnName = "id", nullable = false))
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
+    private Set<Tag> tags = new HashSet<>();
+
+    public ArticleContents(ArticleTitle title, String description, String body, Set<Tag> tags) {
+        this.title = title;
+        this.description = description;
+        this.body = body;
+        this.tags = tags;
+    }
+
+}
