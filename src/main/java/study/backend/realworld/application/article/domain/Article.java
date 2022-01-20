972461c01d7f0fd1c3930abafc5efa1c33e06cea
JanghoPark <pjhsk113@naver.com>
2022-01-20 14:28:40 +0900

    refactor: JPA Auditing 기능 분리
    - BaseTime을 상속하도록 변경

--- src/main/java/study/backend/realworld/application/article/domain/Article.java
+++ src/main/java/study/backend/realworld/application/article/domain/Article.java
@@ -3,22 +3,17 @@ package study.backend.realworld.application.article.domain;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import study.backend.realworld.application.user.domain.User;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
 @Table(name = "articles")
 @Getter
 @Entity
-@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Article {
+public class Article extends BaseTime {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Long id;
@@ -37,14 +32,6 @@ public class Article {
     private Set<User> userFavorited = new HashSet<>();
 
     private boolean favorited = false;
-
-    @CreatedDate
-    @Column(name = "created_at")
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
 
     // TODO Comments
 
