f50c6e48fda4e853525b5f991da90ba7d1151f7a
JanghoPark <pjhsk113@naver.com>
2022-01-21 15:56:31 +0900

    feat: 글 목록을 반환하기 위한 응답 객체 추가

---
+++ src/main/java/study/backend/realworld/application/article/dto/response/MultipleArticlesResponse.java
@@ -0,0 +1,28 @@
+package study.backend.realworld.application.article.dto.response;
+
+import org.springframework.data.domain.Page;
+import study.backend.realworld.application.article.domain.Article;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MultipleArticlesResponse {
+    private List<ArticleResponse> articles;
+    private int articlesCount;
+
+    private MultipleArticlesResponse(List<ArticleResponse> articles, int articlesCount) {
+        this.articles = articles;
+        this.articlesCount = articlesCount;
+    }
+
+    public static MultipleArticlesResponse from(Page<Article> articles) {
+        List<ArticleResponse> articleList = articlesToList(articles);
+        return new MultipleArticlesResponse(articleList, articleList.size());
+    }
+
+    private static List<ArticleResponse> articlesToList(Page<Article> articles) {
+        return articles.map(ArticleResponse::of)
+                .stream()
+                .collect(Collectors.toList());
+    }
+}
