3d64410d9c69eb74665457af0ec89774ef36b515
JanghoPark <pjhsk113@naver.com>
2022-01-27 16:33:43 +0900

    feat: 댓글 목록 조회 비즈니스 로직 추가

--- src/main/java/study/backend/realworld/application/article/application/CommentService.java
+++ src/main/java/study/backend/realworld/application/article/application/CommentService.java
@@ -9,6 +9,8 @@ import study.backend.realworld.application.user.domain.User;
 import study.backend.realworld.application.user.exception.UserNotFountException;
 import study.backend.realworld.application.user.repository.UserRepository;
 
+import java.util.Set;
+
 @Service
 @RequiredArgsConstructor
 public class CommentService {
@@ -22,4 +24,13 @@ public class CommentService {
 
         return findUser.addCommentToArticle(findArticle, body);
     }
+
+    public Set<Comment> findAllCommentsOnArticle(User user, String slug) throws UserNotFountException {
+        User findUser = userRepository.findById(user.getId())
+                .orElseThrow(UserNotFountException::new);
+        Article findArticle = articleRepository.findArticleByContentsTitleSlug(slug);
+
+        return findUser.getArticleComments(findArticle);
+    }
+
 }
