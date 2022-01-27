f00b86f55653a91b0b757aa26bb01223255e49d6
JanghoPark <pjhsk113@naver.com>
2022-01-27 16:30:35 +0900

    feat: 댓글 리스트 반환 객체 추가

---
+++ src/main/java/study/backend/realworld/application/article/dto/response/MultipleCommentResponse.java
@@ -0,0 +1,22 @@
+package study.backend.realworld.application.article.dto.response;
+
+import study.backend.realworld.application.article.domain.Comment;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class MultipleCommentResponse {
+    private List<CommentResponse> comments;
+
+    private MultipleCommentResponse(List<CommentResponse> comments) {
+        this.comments = comments;
+    }
+
+    public static MultipleCommentResponse from(Set<Comment> comments) {
+        List<CommentResponse> convertComments = comments.stream()
+                .map(CommentResponse::from)
+                .collect(Collectors.toList());
+        return new MultipleCommentResponse(convertComments);
+    }
+}
