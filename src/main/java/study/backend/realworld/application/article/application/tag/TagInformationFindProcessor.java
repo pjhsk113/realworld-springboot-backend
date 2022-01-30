f1f236003504e85e40b39231e09b32c768947678
JanghoPark <pjhsk113@naver.com>
2022-01-30 16:19:40 +0900

    feat: 요청 정보와 매핑되는 Tag 목록을 반환하는 메서드 추가

--- src/main/java/study/backend/realworld/application/article/application/tag/TagInformationFindProcessor.java
+++ src/main/java/study/backend/realworld/application/article/application/tag/TagInformationFindProcessor.java
@@ -2,12 +2,14 @@ package study.backend.realworld.application.article.application.tag;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import study.backend.realworld.application.article.domain.ArticleContents;
 import study.backend.realworld.application.article.domain.Tag;
 import study.backend.realworld.application.article.repository.TagRepository;
 
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -21,5 +23,12 @@ public class TagInformationFindProcessor {
 
     public Optional<Tag> findTagValue(String tagName) {
         return tagRepository.findFirstByValue(tagName);
+    }
+
+    public Set<Tag> findMappedTags(ArticleContents contents) {
+        return contents.getTags().stream()
+                .map(tag -> tagRepository.findFirstByValue(String.valueOf(tag))
+                        .orElse(tag))
+                .collect(Collectors.toSet());
     }
 }
