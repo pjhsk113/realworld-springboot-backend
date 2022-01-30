7006c1a12c62fb54d573712a9f0801abbbf59741
JanghoPark <pjhsk113@naver.com>
2022-01-30 16:14:39 +0900

    feat: CUD를 담당하는 비즈니스 클래스 생성
    - 비즈니스 로직과 세부 구현을 분리해 비즈니스 로직에서 흐름 파악이 용이하도록 변경

---
+++ src/main/java/study/backend/realworld/application/article/application/ArticleCommandExecutor.java
@@ -0,0 +1,62 @@
+package study.backend.realworld.application.article.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import study.backend.realworld.application.article.application.tag.TagInformationFindProcessor;
+import study.backend.realworld.application.article.domain.Article;
+import study.backend.realworld.application.article.domain.ArticleContents;
+import study.backend.realworld.application.article.domain.Tag;
+import study.backend.realworld.application.article.domain.model.ArticleUpdateModel;
+import study.backend.realworld.application.user.application.UserFindProcessor;
+import study.backend.realworld.application.user.domain.User;
+import study.backend.realworld.application.user.exception.UserNotFountException;
+
+import javax.security.sasl.AuthenticationException;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleCommandExecutor {
+
+    private final TagInformationFindProcessor tagInformationFindProcessor;
+    private final ArticleCommandManager articleCommandManager;
+    private final ArticleFindProcessor articleFindProcessor;
+    private final UserFindProcessor userFindProcessor;
+
+    public Article createArticle(User user, ArticleContents contents) throws UserNotFountException {
+        Set<Tag> mappedTags = tagInformationFindProcessor.findMappedTags(contents);
+
+        ArticleContents reloadContents = contents.refreshTags(mappedTags);
+
+        User findUser = userFindProcessor.findById(user.getId());
+
+        return articleCommandManager.saveArticle(findUser, reloadContents);
+    }
+
+    public Article updateArticle(User user, String slug, ArticleUpdateModel updateArticleInformation) throws UserNotFountException, AuthenticationException {
+        Article findArticle = articleFindProcessor.findBySlug(slug);
+
+        return userFindProcessor.findById(user.getId())
+                .updateArticle(findArticle, updateArticleInformation);
+    }
+
+
+    public void deleteArticleBySlug(User user, String slug) throws UserNotFountException {
+        User findUser = userFindProcessor.findById(user.getId());
+        articleCommandManager.deleteArticle(findUser, slug);
+    }
+
+    public Article favoriteArticle(User user, String slug) throws UserNotFountException {
+        Article findArticle = articleFindProcessor.findBySlug(slug);
+
+        return userFindProcessor.findById(user.getId())
+                .favoriteArticle(findArticle);
+    }
+
+    public Article unfavoriteArticle(User user, String slug) throws UserNotFountException {
+        Article findArticle = articleFindProcessor.findBySlug(slug);
+
+        return userFindProcessor.findById(user.getId())
+                .unfavoriteArticle(findArticle);
+    }
+}
---
+++ src/main/java/study/backend/realworld/application/article/application/ArticleCommandManager.java
@@ -0,0 +1,25 @@
+package study.backend.realworld.application.article.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import study.backend.realworld.application.article.domain.Article;
+import study.backend.realworld.application.article.domain.ArticleContents;
+import study.backend.realworld.application.article.repository.ArticleRepository;
+import study.backend.realworld.application.user.domain.User;
+
+@Component
+@RequiredArgsConstructor
+public class ArticleCommandManager {
+
+    private final ArticleRepository articleRepository;
+
+    public Article saveArticle(User user, ArticleContents contents) {
+        Article article = new Article(user, contents);
+        return articleRepository.save(article);
+    }
+
+    public void deleteArticle(User user, String slug) {
+        articleRepository.deleteArticleByAuthorAndContentsTitleSlug(user, slug);
+    }
+
+}
