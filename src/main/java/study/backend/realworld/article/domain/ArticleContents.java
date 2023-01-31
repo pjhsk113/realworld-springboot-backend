887e75bd2a275d43de412c9aed0e09159dbfbf18
JanghoPark <pjhsk113@naver.com>
2023-01-31 23:23:21 +0900

    refactor: 헥사고날 아키텍쳐 적용해보기 - domain

---
+++ src/main/java/study/backend/realworld/article/domain/ArticleContents.java
@@ -0,0 +1,68 @@
+package study.backend.realworld.article.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import study.backend.realworld.application.article.domain.model.ArticleUpdateModel;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class ArticleContents {
+
+    @Embedded
+    private ArticleTitle title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String body;
+
+    @JoinTable(name = "articles_tags",
+            joinColumns = @JoinColumn(name = "article_id", referencedColumnName = "id", nullable = false),
+            inverseJoinColumns = @JoinColumn(name = "tag_id", referencedColumnName = "id", nullable = false))
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
+    private Set<Tag> tags = new HashSet<>();
+
+    public ArticleContents(ArticleTitle title, String description, String body, Set<Tag> tags) {
+        this.title = title;
+        this.description = description;
+        this.body = body;
+        this.tags = tags;
+    }
+
+    public Set<String> convertTagTypeToString() {
+        return tags.stream()
+                .map(Tag::toString)
+                .collect(Collectors.toSet());
+    }
+
+    public ArticleContents refreshTags(Set<Tag> tags) {
+        return new ArticleContents(
+                this.title,
+                this.description,
+                this.body,
+                tags
+        );
+    }
+
+    public void updateArticleContents(ArticleUpdateModel updateModel) {
+        if (updateModel.getTitle() != null) {
+            title =  updateModel.getTitle();
+        }
+
+        if (updateModel.getDescription() != null) {
+            description =  updateModel.getDescription();
+        }
+
+        if (updateModel.getBody() != null) {
+            body =  updateModel.getBody();
+        }
+    }
+}
